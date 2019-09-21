fee32706e8d6cf73929b187cd9bd16132bebbb7c
Fabian von Feilitzsch <fabian@fabianism.us>
2019-09-21 13:21:30 -0400

    variety of improvemnts

--- demo_setup.sh
+++ demo_setup.sh
@@ -6,8 +6,10 @@ pushd kubernetes
 popd
 
 pushd traditional/minecraft
+sed -i -e 's/server.jar/spigot.jar/' templates/minecraft.service.j2
+git add templates/minecraft.service.j2
+git commit -m "Breaking the role for my demo"
+git push origin master
+
 (molecule converge && molecule verify)&
 (molecule converge -s vagrant && molecule verify -s vagrant)&
-popd
-
-((flatpak run com.mojang.Minecraft)&)&
