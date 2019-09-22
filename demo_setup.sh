bfc6ec40279780c57da44419276662449a924223
Fabian von Feilitzsch <fabian@fabianism.us>
2019-09-22 13:43:47 -0400

    Destroy stuff during setup script

--- demo_setup.sh
+++ demo_setup.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/bash
 
 pushd kubernetes
+molecule destroy --all
 (molecule converge && molecule verify)&
 (molecule converge -s operator && molecule verify -s operator)&
 popd
 
 pushd traditional/minecraft
+molecule destroy --all
+
 sed -i -e 's/server.jar/spigot.jar/' templates/minecraft.service.j2
 git add templates/minecraft.service.j2
 git commit -m "Breaking the role for my demo"
